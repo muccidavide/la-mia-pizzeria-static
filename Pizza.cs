50ed1217e1606eaf1004ef393b80a5a87f206e79
muccidavide <davidemucci0804@gmail.com>
2022-10-03 14:49:07 +0200

    Aggiungere i file di progetto.

---
+++ Pizza.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace la_mia_pizzeria_static
+{
+    public class Pizza
+    {
+        public Pizza(string name, string description, string image, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+        }
+
+        public BigInteger Id { get; private set; }
+        public string  Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public decimal Price { get; set; }
+
+        
+    }
+}
